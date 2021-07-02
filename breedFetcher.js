c7a67db569e86119385192b1198368eea7446877
judelt <mendez.puig@gmail.com>
2021-07-02 13:37:03 -0700

    refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,22 @@
 const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
-  const apiURL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-  request(apiURL, function(error, response, body) {
-    
-    const data = JSON.parse(body);
-    if (data.length === 0) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, resp, body) => {
+    if (error) {
       return callback(error, null);
+    }
+
+    const data = JSON.parse(body);
+    const breed = data[0];
+
+    if (breed) {
+      callback(null, breed.description);
     } else {
-      callback(error, data[0].description);
+      callback('Breed Not Found');
     }
   });
 };
-  
+
 module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const breedName = process.argv[2];
 fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
-  } else if (desc === null) {
-    console.log('incorrect breed');
   } else {
-    console.log(desc[0].description);
+    console.log('Description: ', desc);
   }
 });
